24dfce3ea01566ccff07880ffc74dd05ca7e350a
Aldosh <alanshalkey@gmail.com>
2018-07-08 21:02:21 -0600

    adding transporation scheduler

---
+++ assets/javascript/TransportationSchedulerLogic.js
@@ -0,0 +1,118 @@
+var config = {
+  apiKey: "AIzaSyDchSfg2u3rdf5MR769fjt7jN5SVb68mJM",
+  authDomain: "publictransportationscheduler.firebaseapp.com",
+  databaseURL: "https://publictransportationscheduler.firebaseio.com",
+  projectId: "publictransportationscheduler",
+  storageBucket: "publictransportationscheduler.appspot.com",
+  messagingSenderId: "210194199920"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$('#submit-button').click(function(event) {
+
+  event.preventDefault();
+
+  var serviceName = $('#service-name').val().trim();
+  var serviceType = $('#service-type').val().trim();
+  var destination = $('#destination').val().trim();
+  var firstArrival = $('#first-arrival').val().trim();
+  var frequency = $('#frequency').val().trim();
+
+  var newService = {
+    sName: serviceName,
+    sType: serviceType,
+    sDestination: destination,
+    sFirstArrival: firstArrival,
+    sFrequency: frequency
+  }
+  justPushed = database.ref().push(newService);
+
+  var removeButton = $('<button>').text('remove')
+                                  .addClass('btn remove-button')
+                                  .attr('id', justPushed.key)
+                                  .click(function(){
+                                  database.ref().child($(this).attr('id')).remove();
+                                  $(this).remove();
+                                  $('#row' + $(this).attr('id')).remove();
+});
+  $('#service-name').val('');
+  $('#service-type').val('');
+  $('#destination').val('');
+  $('#first-arrival').val('');
+  $('#frequency').val('');
+
+  database.ref().on('child_added', function(childSnapshot) {
+
+    var serviceName = childSnapshot.val().sName;
+    var serviceType = childSnapshot.val().sType;
+    var destination = childSnapshot.val().sDestination;
+    var firstArrival = childSnapshot.val().sFirstArrival;
+    var frequency = childSnapshot.val().sFrequency;
+
+    var convertedFirstArrival = moment(firstArrival, 'H:mm').subtract(1, 'years');
+
+
+    var timeDifference = moment().diff(moment(convertedFirstArrival), 'minutes');
+
+    timeRemainder = timeDifference % frequency;
+
+    var minutesAway = frequency - timeRemainder;
+    var nextArrivalTime = moment().add(minutesAway, 'minutes').format('hh:mm A');
+
+    var newRow = $('<tr>').attr('id', 'row' + removeButton.attr('id')).append(
+      $('<td>').text(serviceName),
+      $('<td>').text(serviceType),
+      $('<td>').text(destination),
+      $('<td>').text(frequency),
+      $('<td>').text(nextArrivalTime),
+      $('<td>').text(minutesAway),
+      removeButton
+    );
+
+    $('#current-schedule').append(newRow);
+
+    // keep from firing child added events multiple times after adding the first child 
+    //(called twice second time, three times third time, etc)
+
+    database.ref().off();
+});
+  
+});
+
+//   database.ref().on('child_added', function(childSnapshot) {
+
+//     var serviceName = childSnapshot.val().sName;
+//     var serviceType = childSnapshot.val().sType;
+//     var destination = childSnapshot.val().sDestination;
+//     var firstArrival = childSnapshot.val().sFirstArrival;
+//     var frequency = childSnapshot.val().sFrequency;
+
+//     var convertedFirstArrival = moment(firstArrival, 'H:mm').subtract(1, 'years');
+
+
+//     var timeDifference = moment().diff(moment(convertedFirstArrival), 'minutes');
+
+//     timeRemainder = timeDifference % frequency;
+
+//     var minutesAway = frequency - timeRemainder;
+//     var nextArrivalTime = moment().add(minutesAway, 'minutes').format('hh:mm A');
+
+//     var newRow = $('<tr>').attr('id', 'row' + removeButton.attr('id')).append(
+//       $('<td>').text(serviceName),
+//       $('<td>').text(serviceType),
+//       $('<td>').text(destination),
+//       $('<td>').text(frequency),
+//       $('<td>').text(nextArrivalTime),
+//       $('<td>').text(minutesAway),
+//       removeButton
+//     );
+//     $('#current-schedule').append(newRow);
+
+//     // keep from firing child added events multiple times after adding the first child 
+//     //(called twice second time, three times third time, etc)
+
+//     database.ref().off();
+// });
